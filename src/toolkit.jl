c3a7f386a4e7bd31bb861e7f8cfd94425cfda3d0
blargg <tomjankauski@gmail.com>
2022-01-23 20:50:58 -0800

    Basic implementation of partial_cor

--- src/toolkit.jl
+++ src/toolkit.jl
@@ -1,9 +1,15 @@
 module toolkit
 
-greet() = print("Hello World!")
+using GLM, DataFrames, Statistics
 
-plusone(x) = x + 1
+function partial_cor(df::AbstractDataFrame, x::Symbol, y::Symbol, z::AbstractVector{Symbol})
+    z_to_y = lm(Term(y) ~ reduce(+, map(Term, z)),df)
+    z_to_x = lm(Term(x) ~ reduce(+, map(Term, z)),df)
+    y_next = df[:,y] - predict(z_to_y)
+    x_next = df[:,x] - predict(z_to_x)
+    return cor(y_next, x_next)
+end
 
-ex() = print("ex")
+export partial_cor
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
 using Test
-import toolkit
+using toolkit, DataFrames
+
 
 @testset "Example" begin
-    @test toolkit.plusone(1) == 2
+    df = DataFrame(x=[1,2,3], y=[10,20,30], z=[1,2,3])
+    # TODO test that this is near 0
+    partial_cor(df, :x, :y, [:z])
 end # testset Example
