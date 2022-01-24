bad22c2b301bd8fa3e4801aef3c425ff11140ac0
blargg <tomjankauski@gmail.com>
2022-01-23 21:05:44 -0800

    Add better test for partial_cor

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,11 @@ using Test
 using toolkit, DataFrames
 
 
-@testset "Example" begin
-    df = DataFrame(x=[1,2,3], y=[10,20,30], z=[1,2,3])
-    # TODO test that this is near 0
-    partial_cor(df, :x, :y, [:z])
+@testset "partial_cor" begin
+    x=1:100
+    y=map(x->x*x, x)
+    df = DataFrame(x=x, y=y, z=x)
+    @test isapprox(0,
+                   partial_cor(df, :x, :y, [:z]),
+                   atol=0.1)
 end # testset Example
